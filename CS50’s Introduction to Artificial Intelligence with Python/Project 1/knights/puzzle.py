e2cd0cfef6ae2a0f832a296ac34ede5f485c7482
Mohit <mohitvwagh@gmail.com>
2020-06-30 02:04:28 +0400

    finished project 1 knights

--- CS50’s Introduction to Artificial Intelligence with Python/Project 1/knights/puzzle.py
+++ CS50’s Introduction to Artificial Intelligence with Python/Project 1/knights/puzzle.py
@@ -9,34 +9,57 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+generic_knowledge = And(Biconditional(AKnight, Not(AKnave)), Biconditional(BKnight, Not(BKnave)),
+                        Biconditional(CKnight, Not(CKnave)))
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Implication(AKnight, And(AKnight, AKnave)), Implication(AKnight, Not(And(AKnight, AKnave)))
 )
+
+knowledge0.add(generic_knowledge)
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+A_says_puzzle1 = And(AKnave, BKnave)
+
 knowledge1 = And(
-    # TODO
+    Implication(AKnight, A_says_puzzle1), Implication(AKnave, Not(A_says_puzzle1)), Or(BKnave, BKnight)
 )
+
+knowledge1.add(generic_knowledge)
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+A_says_puzzle2 = Biconditional(AKnave, BKnave)
+B_says_puzzle2 = Biconditional(AKnight, BKnave)
+
 knowledge2 = And(
-    # TODO
+    Implication(AKnight, A_says_puzzle2), Implication(AKnave, Not(A_says_puzzle2)),
+    Implication(BKnight, B_says_puzzle2), Implication(BKnave, B_says_puzzle2)
 )
+
+knowledge2.add(generic_knowledge)
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+A_says_puzzle3 = Or(AKnight, AKnave)
+B_says_puzzle3 = And(Implication(A_says_puzzle3, AKnave), CKnave)
+C_says_puzzle3 = AKnight
+
 knowledge3 = And(
-    # TODO
+    Implication(AKnight, A_says_puzzle3), Implication(AKnave, Not(A_says_puzzle3)),
+    Implication(BKnight, B_says_puzzle3), Implication(BKnave, Not(B_says_puzzle3)),
+    Implication(CKnight, C_says_puzzle3), Implication(CKnave, Not(C_says_puzzle3))
 )
+
+knowledge3.add(generic_knowledge)
 
 
 def main():
