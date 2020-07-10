69f03534b1758c448a36c854e49db53c7a6a9d62
Mohit <mohitvwagh@gmail.com>
2020-07-10 05:07:13 +0400

    finished project 2 pagerank.py

--- CS50’s Introduction to Artificial Intelligence with Python/Project 2/pagerank/pagerank.py
+++ CS50’s Introduction to Artificial Intelligence with Python/Project 2/pagerank/pagerank.py
@@ -57,7 +57,14 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    probability_distribution = dict()
+    no_of_pages = len(corpus)
+    for k in corpus.keys():
+        probability_distribution[k] = (1 - damping_factor) / no_of_pages
+    no_of_links_from_page = len(corpus[page])
+    for v in corpus[page]:
+        probability_distribution[v] = probability_distribution[v] + damping_factor / no_of_links_from_page
+    return probability_distribution
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +76,19 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    page_rank = dict(corpus)
+    for k in page_rank:
+        page_rank[k] = 0
+    current_page = random.choice(list(corpus))
+    page_rank[current_page] = 1
+    for _ in range(1, n):
+        current_transition_model = transition_model(corpus, current_page, damping_factor)
+        current_page = random.choices(population=list(current_transition_model),
+                                      weights=list(current_transition_model.values()))[0]
+        page_rank[current_page] = page_rank[current_page] + 1
+    for k in page_rank:
+        page_rank[k] = page_rank[k] / n
+    return page_rank
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +100,26 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    number_of_pages = len(corpus)
+    old_page_rank = dict(corpus)
+    for k in old_page_rank:
+        old_page_rank[k] = 1 / number_of_pages
+    new_page_rank = dict(old_page_rank)
+    max_error = 0.001
+    error = 1
+    while error > max_error:
+        error = 0
+        old_page_rank = dict(new_page_rank)
+        for k in new_page_rank:
+            new_page_rank[k] = 0
+            for i in corpus:
+                if k in corpus[i]:
+                    new_page_rank[k] = new_page_rank[k] + old_page_rank[i] / len(corpus[i])
+            new_page_rank[k] = damping_factor * new_page_rank[k] + (1 - damping_factor) / number_of_pages
+            current_error = abs(new_page_rank[k] - old_page_rank[k])
+            if current_error > error:
+                error = current_error
+    return new_page_rank
 
 
 if __name__ == "__main__":
